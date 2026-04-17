8dc31262814f635d0e4ee989f4165fd945775e42
Thomas DUPRESSOIR <tomtoomuch@users.noreply.github.com>
2026-04-17 09:17:40 +0200

    oops

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import plotly.express as px
 import pandas as pd
 
-données = pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vSC4KusfFzvOsr8WJRgozzsCxrELW4G4PopUkiDbvrrV2lg0S19-zeryp02MC9WYSVBuzGCUtn8ucZW/pub?output=csv')
+donnees = pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vSC4KusfFzvOsr8WJRgozzsCxrELW4G4PopUkiDbvrrV2lg0S19-zeryp02MC9WYSVBuzGCUtn8ucZW/pub?output=csv')
 
-figure = px.pie(données, values='qte', names='region', title='quantité vendue par région')
+figure = px.pie(donnees, values='qte', names='region', title='Quantité vendue par région')
 
 figure.write_html('ventes-par-region.html')
 
