75a7293fd4b476d523941b7931bf3ff9f3580b92
Thomas DUPRESSOIR <tomtoomuch@users.noreply.github.com>
2026-04-17 18:45:02 +0200

    reassort

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 import plotly.express as px
 import pandas as pd
 
-donnees = pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vSC4KusfFzvOsr8WJRgozzsCxrELW4G4PopUkiDbvrrV2lg0S19-zeryp02MC9WYSVBuzGCUtn8ucZW/pub?output=csv')
+données = pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vSC4KusfFzvOsr8WJRgozzsCxrELW4G4PopUkiDbvrrV2lg0S19-zeryp02MC9WYSVBuzGCUtn8ucZW/pub?output=csv')
 
-figure = px.pie(donnees, values='qte', names='region', title='Quantité vendue par région')
+# Quantité vendue par région
+qte_region = px.pie(données, values='qte', names='region', title='quantité vendue par région')
+# Ventes par produit
+qte_produit = px.pie(données, values='qte', names='produit', title='ventes par produit')
+# CA par produit
+données['ca'] = données['prix'] * données['qte']
+ca_produit = px.pie(données, values='ca', names='produit', title='CA par produit')
 
-figure.write_html('ventes-par-region.html')
+qte_region.write_html('ventes-par-region.html')
+qte_produit.write_html('ventes-par-produit.html')
+ca_produit.write_html('ca-par-produit.html')
 
-print('ventes-par-région.html généré avec succès !')
+print('ventes-par-région.html, ventes-par-produit.html, ca-par-produit.html générés avec succès !')
